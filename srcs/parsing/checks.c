38622f08add2cf44da865274dbe4164e7ece6835
ckurt <ckurt@student.42lyon.fr>
2021-03-15 10:31:51 +0100

    patched shit

--- srcs/parsing/checks.c
+++ srcs/parsing/checks.c
@@ -6,7 +6,7 @@
 /*   By: ckurt <ckurt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 13:19:09 by ckurt             #+#    #+#             */
-/*   Updated: 2021/03/15 10:25:26 by ckurt            ###   ########lyon.fr   */
+/*   Updated: 2021/03/15 10:27:34 by ckurt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,20 +54,20 @@ int	verify_resolution(t_engine *engine, int save)
 	int	max_y;
 
 	mlx_get_screen_size(engine->mlx, &max_x, &max_y);
-	if (DEBUG)
-		printf("x: %d, y: %d\n", engine->size_x, engine->size_y);
+	// if (DEBUG)
+	// 	printf("x: %d, y: %d\n", engine->size_x, engine->size_y);
 	if (engine->size_x > 16383 || engine->size_y > 16383)
 		close_minirt("Resolution too high (set limit of 16384)");
+	if (engine->size_x <= 0 || engine->size_y <= 0)
+		close_minirt("Invalid resolution");
 	if (!save)
 	{
-		if (engine->size_x <= 0 || engine->size_y <= 0)
-			close_minirt("Invalid resolution");
 		if (engine->size_x > max_x)
 			engine->size_x = max_x;
 		if (engine->size_y > max_y)
 			engine->size_y = max_y;
 	}
-	if (DEBUG)
-		printf("x: %d, y: %d\n", engine->size_x, engine->size_y);
+	// if (DEBUG)
+	// 	printf("x: %d, y: %d\n", engine->size_x, engine->size_y);
 	return (1);
 }
